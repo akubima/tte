302afa4268fc4bb03681fb867402560fdd4df684
Bima <63215668+akubima@users.noreply.github.com>
2025-05-09 10:43:41 +0700

    feat: :sparkles: Create certificaete function

--- tte.sh
+++ tte.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# NOTE: 
-# The character '|' and ';' are used as delimiter in this bash scripts and should not be used in any user input or inside any data except it become the delimiter itself.
+# =========
+# IMPORTANT NOTE: Character '|' And ';' is used as a delimiter in this bash script and should not be used in user input or within any data unless the character is a delimiter itself.
+# =========
 
+# =========================== GLOBAL VARIABLES DECLARATION ===========================
 PATH_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PATH_CERTS_DIR="$PATH_SCRIPT_DIR/.certs"
 PATH_SIGNER="$PATH_SCRIPT_DIR/.resources/open-pdf-sign.jar"
@@ -14,14 +16,17 @@ UI_WINDOW_SIZE_ALERT=(300 100)
 
 declare -A DATA_MAP_CERTIFICATES_AVAILABLE
 DATA_CERTIFICATES_AVAILABLE=()
-
-mkdir -p "$PATH_CERTS_DIR"
-touch "$PATH_CONFIG_FILE"
+# =========================== END OF GLOBAL VARIABLES DECLARATION =====================
 
 # =========================== FUNCTIONS DEFINITION ===========================
+FN_PreRun () {
+    mkdir -p "$PATH_CERTS_DIR" || return 1
+    touch "$PATH_CONFIG_FILE" || return 1
+}
+
 FN_ReadConfig () {
     local KEY="$1"
-    grep "^$KEY=" "$PATH_CONFIG_FILE" 2>/dev/null | cut -d '=' -f 2
+    grep "^$KEY=" "$PATH_CONFIG_FILE" 2> /dev/null | cut -d '=' -f 2
 }
 
 FN_WriteConfig () {
@@ -38,26 +43,22 @@ FN_WriteConfig () {
 
 FN_PromptEULA () {
     if [ ! -f "$PATH_EULA_FILE" ]; then
-        FN_ShowError "" "EULA tidak dapat ditemukan!" 1
+        FN_ShowError "" "EULA tidak dapat ditemukan!"
         return 1
     fi
 
     zenity --text-info --title="Perjanjian Lisensi Pengguna Akhir (EULA)" --filename="$PATH_EULA_FILE" --checkbox="Saya menyetujui ketentuan dalam EULA ini." --html --width=${UI_WINDOW_SIZE_GENERAL[0]} --height=${UI_WINDOW_SIZE_GENERAL[1]}
 
     if [ $? -eq 0 ]; then
-        FN_WriteConfig "EULA_AGREED" "TRUE" && return 0
+        FN_WriteConfig "EULA_AGREED" "TRUE" && return 0 || return 1
     fi
-
-    return 1
 }
 
 FN_SelectExistingOrCreateNewCert () {
     zenity --list --radiolist --title="Tentukan Pilihan" --text="Gunakan sertifikat yang ada atau buat baru?" --column="#" --column="Nomor" --column="Pilihan" --print-column=2 --width=${UI_WINDOW_SIZE_GENERAL[0]} --height=${UI_WINDOW_SIZE_GENERAL[1]} "TRUE" 1 "Gunakan sertifikat yang ada" "FALSE" 2 "Buat sertifikat baru"
 }
 
-# Menampilkan dialog pemilihan file PDF.
 FN_SelectPDFFile () {
-    # Karena fungsi bash cuma bisa mengembalikan numeric integer value, maka langsung aja perintah tanpa dibungkus $(). 
     zenity --file-selection --title="Pilih File PDF Untuk Ditandatangani" --file-filter="*.pdf" --width=${UI_WINDOW_SIZE_GENERAL[0]} --height=${UI_WINDOW_SIZE_GENERAL[1]}
 }
 
@@ -67,17 +68,14 @@ FN_StorePDFFilePath () {
     zenity --file-selection --save --confirm-overwrite --title="Pilih Lokasi Penyimpanan Dokumen" --filename="${PDF_FILE%.pdf}-signed.pdf" --file-filter="*.pdf"
 }
 
-# Penmeriksa tipe file.
 FN_CheckFileType () {
     local FILE="$1"
     local DESIRED_TYPE="$2"
     local FILE_TYPE=$(file --mime-type -b "$FILE")
 
-    # IF statement dengan format [ condition ] && true || false
     [ "$FILE_TYPE" = "$DESIRED_TYPE" ] && return 0 || return 1
 }
 
-# Menampilkan close-ended question dialog.
 FN_ShowQuestion () {
     local TITLE="${1:-"Konfirmasi"}"
     local MESSSAGE="$2"
@@ -90,11 +88,8 @@ FN_ShowQuestion () {
 FN_ShowError () {
     local TITLE="${1:-"Terjadi Kesalahan"}"
     local MESSSAGE="${2:-"Terjadi kesalahan yang tidak diketahui."}"
-    local THEN_EXIT="${3:-0}"
 
     zenity --error --title="$TITLE" --text="$MESSSAGE" --width=${UI_WINDOW_SIZE_ALERT[0]} --height=${UI_WINDOW_SIZE_ALERT[1]}
-
-    [ "$THEN_EXIT" = 1 ] && exit 1
 }
 
 FN_ShowInfo () {
@@ -104,7 +99,63 @@ FN_ShowInfo () {
     zenity --info --title="$TITLE" --text="$MESSSAGE" --width=${UI_WINDOW_SIZE_ALERT[0]} --height=${UI_WINDOW_SIZE_ALERT[1]}
 }
 
-# Mendapatkan sertifikat yang tersedia dan valid di dalam direktori ./certs.
+FN_CreateCertificate () {
+    local MAN_CN MAN_EMAIL OPS_ORG MAN_COUNTRY FORM_RESULT CONFIG_LAST_CERT_ID CURRENT_CERT_ID CURRENT_CERT_NAME CURRENT_CERT_DIR
+    
+    while [[ -z "$MAN_CN" || -z "$MAN_EMAIL" || -z "$MAN_COUNTRY" ]]; do
+        FORM_RESULT=$(zenity --forms \
+            --title="Buat Sertifikat Baru" \
+            --separator="|" \
+            --text="Masukkan informasi untuk sertifikat digital:" \
+            --width=${UI_WINDOW_SIZE_GENERAL[0]} --height=${UI_WINDOW_SIZE_GENERAL[1]} \
+            --add-entry="Nama Lengkap (CN)" \
+            --add-entry="Email" \
+            --add-entry="Organisasi (O) (opsional)" \
+            --add-combo="Negara (C)" \
+            --combo-values="ID|AR|AU|BR|CA|CN|FR|DE|IN|IT|JP|MX|RU|SA|ZA|KR|TR|GB|US|EU"
+        )
+        
+        [ $? -ne 0 ] && return 1
+
+        IFS="|" read -r MAN_CN MAN_EMAIL OPS_ORG MAN_COUNTRY <<< "$FORM_RESULT"
+
+        if [[ -z "$MAN_CN" || -z "$MAN_EMAIL" || -z "$MAN_COUNTRY" ]]; then
+            FN_ShowError "Input Tidak Valid" "Nama Lengkap (CN), Email, dan Negara (C) harus diisi!"
+            continue
+        fi
+
+        for FILLING_CONTENT in "$MAN_CN" "$MAN_EMAIL" "$MAN_ORG" "$MAN_COUNTRY"; do
+            if [[ "$FILLING_CONTENT" == *"|"* || "$FILLING_CONTENT" == *";"* ]]; then
+                FN_ShowError "Karakter Tidak Valid" "Input tidak boleh mengandung karakter '|' atau ';'."
+                continue 2
+            fi
+        done
+    done
+
+    CONFIG_LAST_CERT_ID=$(FN_ReadConfig "LAST_CERT_ID") || return 0
+    [ -z "$CONFIG_LAST_CERT_ID" ] && CONFIG_LAST_CERT_ID=0
+    CURRENT_CERT_ID=$((CONFIG_LAST_CERT_ID + 1))
+
+    CURRENT_CERT_DIR="$PATH_CERTS_DIR/${CURRENT_CERT_ID}|${MAN_CN}"
+    mkdir -p "$CURRENT_CERT_DIR"
+    if [ $? -ne 0 ]; then
+        FN_ShowError "Gagal Membuat Direktori" "Tidak dapat membuat direktori: $CURRENT_CERT_DIR"
+        return 1
+    fi
+
+    CREATE_CERT_RESULT=$(openssl req -x509 -nodes -days "3650" -newkey rsa:4096 \
+        -keyout "$CURRENT_CERT_DIR/privkey.pem" \
+        -out "$CURRENT_CERT_DIR/fullchain.pem" \
+        -subj "/C=$MAN_COUNTRY/CN=$MAN_CN/emailAddress=$MAN_EMAIL/O=${OPS_ORG:-$MAN_CN}")
+    if [ $? -ne 0 ]; then
+        FN_ShowError "Gagal Membuat Sertifikat" "Tidak dapat membuat sertifikat: $CREATE_CERT_RESULT"
+        return 1
+    fi
+
+    FN_WriteConfig "LAST_CERT_ID" "$CURRENT_CERT_ID" || return 1
+    FN_ShowInfo "Sertifikat Berhasil Dibuat" "Sertifikat dengan common name '$MAN_CN' telah berhasil dibuat!" && return 0
+}
+
 FN_GetAvailableCertificates() {
     
     DATA_CERTIFICATES_AVAILABLE=()
@@ -119,30 +170,28 @@ FN_GetAvailableCertificates() {
 
         # Jika file sertifikat lengkap dan kunci privat ada, tambahkan ke daftar pilihan.
         if [[ -f "$FULLCHAIN_CERT_PATH" && -f "$KEY_CERT_PATH" ]]; then
-                # Ambil nama sertifikat dari nama direktori paling kanan dalam path.
-                local BASENAME=$(basename "$COMMON_NAME_DIR")
+            # Ambil nama sertifikat dari nama direktori paling kanan dalam path.
+            local BASENAME=$(basename "$COMMON_NAME_DIR") || return 1
 
-                # Ambil ID dan nama sertifikat dengan Internal Field Separator (IFS) untuk memisahkan berdasarkan delimiter '|'.
-                local CERT_ID CERT_NAME
-                IFS="|" read -r CERT_ID  CERT_NAME <<< "$BASENAME"
+            # Ambil ID dan nama sertifikat dengan Internal Field Separator (IFS) untuk memisahkan berdasarkan delimiter '|'.
+            local CERT_ID CERT_NAME
+            IFS="|" read -r CERT_ID CERT_NAME <<< "$BASENAME"
 
-                # Tambahkan ke tuple sebagai pilihan sertifikat yang valid.
-                DATA_CERTIFICATES_AVAILABLE+=("FALSE" "$CERT_ID" "$CERT_NAME")
-                
-                # Simpan path sertifikat ke dalam map (array) untuk akses cepat.
-                DATA_MAP_CERTIFICATES_AVAILABLE["$CERT_ID"]="$FULLCHAIN_CERT_PATH;$KEY_CERT_PATH"
-            fi
-        done
+            # Tambahkan ke tuple sebagai pilihan sertifikat yang valid.
+            DATA_CERTIFICATES_AVAILABLE+=("FALSE" "$CERT_ID" "$CERT_NAME")
+            
+            # Simpan path sertifikat ke dalam map (array) untuk akses cepat.
+            DATA_MAP_CERTIFICATES_AVAILABLE["$CERT_ID"]="$FULLCHAIN_CERT_PATH;$KEY_CERT_PATH"
+        fi
+    done
     
     return 0
 }
 
-# Menampilkan daftar sertifikat yang tersedia dan user harus milih satu.
 FN_ShowSelectAvailableCertificates() {
     zenity --list --radiolist --title="Pilih Sertifikat" --text="Pilih sertifikat elektronik yang ingin digunakan untuk menandatangani dokumen." --column="#" --column="ID Sertifikat" --column="Nama Sertifikat" --print-column=2 --width=${UI_WINDOW_SIZE_GENERAL[0]} --height=${UI_WINDOW_SIZE_GENERAL[1]} "${DATA_CERTIFICATES_AVAILABLE[@]}"
 }
 
-# Fungsi penandatanganan dokumen PDF.
 FN_SignPDF() {
     local PDF_FILE="$1"
     local CERT_FILE="$2"
@@ -150,25 +199,22 @@ FN_SignPDF() {
     local OUTPUT_FILE_PATH="$4"
     local RESULT
 
-    echo "Signing $PDF_FILE ..."
-
+    echo "Menandatangani $PDF_FILE ..."
     RESULT=$(java -jar "$PATH_SIGNER" -c "$CERT_FILE" -k "$KEY_FILE"  -i "$PDF_FILE"  -o "$OUTPUT_FILE_PATH" )
     
     if [ $? -eq 0 ]; then
-        echo "Signing OK!"
-
+        echo "Penandatanganan OK!"
         FN_ShowInfo "Berhasil" "File PDF berhasil ditandatangani dan telah disimpan di:\n$OUTPUT_FILE_PATH\n\n$(pdfsig "$OUTPUT_FILE_PATH")"
         return 0
     else
-        echo "Signing FAILED! $RESULT"
-
+        echo "Penandatanganan GAGAL! $RESULT"
         FN_ShowError "" "File PDF gagal ditandatangani.\n\n$RESULT"
         return 1
     fi
 }
 # =========================== END OF FUNCTIONS DEFINITION ===========================
-
 # =========================== BEGINING OF ALGORITHM ===========================
+FN_PreRun || exit 1
 
 EULA="$(FN_ReadConfig "EULA_AGREED")"
 
@@ -179,20 +225,22 @@ fi
 FN_GetAvailableCertificates || exit 1
 
 if [ "${#DATA_CERTIFICATES_AVAILABLE[@]}" -eq 0 ]; then
-    FN_ShowInfo "" "Creating certificate..."
+    FN_CreateCertificate || exit 1
 else
     SIGNATORY_CERT_OPTION=$(FN_SelectExistingOrCreateNewCert) || exit 1
-    [ "$SIGNATORY_CERT_OPTION" = "2" ] && FN_ShowInfo "" "Creating certificate..."
+    
+    if [ "$SIGNATORY_CERT_OPTION" = "2" ]; then 
+        FN_CreateCertificate || exit 1
+    fi
 fi
 
 SELECTED_PDF_FILE=$(FN_SelectPDFFile) || exit 1
 
 [ -z "$SELECTED_PDF_FILE" ] && exit 1;
 
-# Jika file yang dipilih udu PDF atau ndak valid, tampilkan dialog konfirmasi buat minta pengguna untuk milih file lain atau membatalkan operasi.
-while true; do
-    FN_CheckFileType "$SELECTED_PDF_FILE" "application/pdf" && break
-    FN_ShowQuestion "File PDF Tidak Valid" "File yang dipilih\n$SELECTED_PDF_FILE\nbukan file PDF yang valid!\n\nIngin memilih file lain?" && SELECTED_PDF_FILE=$(FN_SelectPDFFile) || exit 1
+while ! FN_CheckFileType "$SELECTED_PDF_FILE" "application/pdf"; do
+    FN_ShowQuestion "File PDF Tidak Valid" "File yang dipilih\n$SELECTED_PDF_FILE\nbukan file PDF yang valid!\n\nIngin memilih file lain?" || exit 1
+    SELECTED_PDF_FILE=$(FN_SelectPDFFile) || exit 1
 done
 
 FN_GetAvailableCertificates && SELECTED_SIGNATORY_CERTIFICATE_ID=$(FN_ShowSelectAvailableCertificates) || exit 1
